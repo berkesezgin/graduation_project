0471ec43a89d5161075583dab2d42867d5194780
Rectiras <soyluberkay@sabanciuniv.edu>
2022-04-10 17:58:17 +0300

    Login/Signup Revised

--- Assets/loginChildUsername.cs
+++ Assets/loginChildUsername.cs
@@ -2,20 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+
 
 public class loginChildUsername : MonoBehaviour
 {
-
     public static string login_child_username;
     public TMP_InputField child_login_username_inputField;
 
+    [SerializeField] private GameObject ChildMenu = null;
+    [SerializeField] private GameObject ChildLoginMenu = null;
     public void setName()
     {
-        //Berke bu fonksiyon login olduğu için mongoDB'de girilen isim daha önceden var mı yok mu onu check etmeli, sonrasında eğer varsa play screenine devam edebilir, eğer yoksa username doesn't exist tarzı bir output vermeli.
-
-
+        
         login_child_username = child_login_username_inputField.text;
         Debug.Log("Child Login Username: " + login_child_username);
+
+        if (login_child_username != "berkay")
+        {
+            TextMeshProUGUI placeholder = (TextMeshProUGUI)child_login_username_inputField.placeholder;
+            placeholder.text = "The Username doesn't exist!";
+        }
+        else
+        {
+
+            //Berke bu fonksiyon login olduğu için mongoDB'de girilen isim daha önceden var mı yok mu onu check etmeli, sonrasında eğer varsa play screenine devam edebilir, eğer yoksa username doesn't exist tarzı bir output vermeli.
+            
+            Debug.Log(login_child_username + " Succesfully Logged in!");
+            ChildLoginMenu.SetActive(false);
+            ChildMenu.SetActive(true);
+        }
+        child_login_username_inputField.text = "";
     }
 
 }
--- Assets/signupChildUsername.cs
+++ Assets/signupChildUsername.cs
@@ -9,13 +9,31 @@ public class signupChildUsername : MonoBehaviour
     public static string signup_child_username;
     public TMP_InputField child_signup_username_inputField;
 
+    [SerializeField] private GameObject ChildLoginMenu = null;
+    [SerializeField] private GameObject ChildSignupMenu = null;
+
     public void setName()
     {
-        //Berke bu fonksiyon signup olduğu için mongoDB'de girilen isim daha önceden var mı yok mu onu check etmeli, sonrasında eğer yoksa yeni bir user yaratmalı, eğer varsa this username already exists tarzı bir output vermeli.
-
+        
 
         signup_child_username = child_signup_username_inputField.text;
         Debug.Log("Child Signup Username: " + signup_child_username);
+
+        if (signup_child_username == "berkay")
+        {
+            TextMeshProUGUI placeholder = (TextMeshProUGUI)child_signup_username_inputField.placeholder;
+            placeholder.text = "The User already exists!";
+        }
+        else
+        {
+            
+            //Berke bu fonksiyon signup olduğu için mongoDB'de girilen isim daha önceden var mı yok mu onu check etmeli, sonrasında eğer yoksa yeni bir user yaratmalı, eğer varsa this username already exists tarzı bir output vermeli.
+            
+            Debug.Log(signup_child_username + " Succesfully signed up!");
+            ChildSignupMenu.SetActive(false);
+            ChildLoginMenu.SetActive(true);
+        }
+        child_signup_username_inputField.text = "";
     }
 
 }
